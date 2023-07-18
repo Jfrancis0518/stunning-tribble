7b62a35dc9e1fa7df33fc91187f4d3b64cc0ae95
Jfrancis0518 <jfrancis0518@gmail.com>
2023-07-18 08:12:35 -0400

    update

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,54 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+
+function saveSchedule() {
+    localStorage.setItem("schedule", JSON.stringify(schedule));
+  }
+  
+  function handleTextareaChange(event) {
+    var textarea = event.target;
+    var timeBlockId = textarea.parentNode.id;
+    var eventText = textarea.value;
+  
+    schedule[timeBlockId] = eventText;
+    saveSchedule();
+  }
+  
+  function handleSaveButtonClick(event) {
+    var saveButton = event.target;
+    var timeBlockId = saveButton.parentNode.id;
+    var eventText = saveButton.parentNode.querySelector(".description").value;
+  
+    schedule[timeBlockId] = eventText;
+    saveSchedule();
+  }
+  
+  function populateTimeBlocks() {
+    var timeBlockElements = document.querySelectorAll(".time-block");
+  
+    timeBlockElements.forEach(function (timeBlockElement) {
+      var timeBlockId = timeBlockElement.id;
+      var textarea = timeBlockElement.querySelector(".description");
+  
+      if (schedule[timeBlockId]) {
+        textarea.value = schedule[timeBlockId];
+      }
+    });
+  }
+  
+  var schedule = JSON.parse(localStorage.getItem("schedule")) || {};
+  
+  var textareas = document.querySelectorAll(".description");
+  var saveButtons = document.querySelectorAll(".saveBtn");
+  
+  textareas.forEach(function (textarea) {
+    textarea.addEventListener("change", handleTextareaChange);
   });
+  
+  saveButtons.forEach(function (saveButton) {
+    saveButton.addEventListener("click", handleSaveButtonClick);
+  });
+  
+  populateTimeBlocks();
+  
